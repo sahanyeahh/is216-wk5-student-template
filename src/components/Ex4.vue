ec09786a976ef5bda75c9eb33c30c6fc862dde06
sahanyeahh <chiranis.w.2024@computing.smu.edu.sg>
2026-09-14 09:01:34 +0800

    week 5

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,7 +3,18 @@ import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
 // TODO: Add Code Here
+const newItem = ref('')
 
+function deleteItem(index) {
+  items.value.splice(index, 1)
+}
+
+function addItem() {
+  if (newItem.value.trim() !== '') {
+    items.value.push(newItem.value)
+    newItem.value = ''   // clear the text box after adding
+  }
+}
 
 </script>
 
@@ -11,6 +22,14 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <button @click="deleteItem(index)">Delete!</button>
+      </li>
+    </ul>
 
+    <input v-model="newItem" @keyup.enter="addItem" />
+    <button @click="addItem">Add!</button>
    
 </template>
--- src/components/Ex5.vue
+++ src/components/Ex5.vue
@@ -1,14 +1,36 @@
 <script setup>
 import axios from 'axios';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
 const moods = ref(['Happy', 'Sad', 'Angry']);
 const subject = ref('');
 const entry = ref('');
 const mood = ref('');
 
+const router = useRouter();
+
 // Add Code Here
+async function submitPost() {
+  try {
+    const response = await axios.post('http://localhost:8000/posts', {
+      subject: subject.value,
+      entry: entry.value,
+      mood: mood.value
+    });
+    console.log('Post created:', response.data);
 
+    // clear the form
+    subject.value = '';
+    entry.value = '';
+    mood.value = '';
 
+    // optionally go back to the main page
+    router.push('/ViewPosts/');
+  } catch (error) {
+    console.error('Error creating post:', error);
+  }
+}
 </script>
 
 <template>
@@ -24,14 +46,17 @@ const mood = ref('');
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
+        <select v-model='mood'>
+            <option value='' disabled>-- Select a mood --</option>
+            <option v-for='m in moods' :key='m' :value='m'>{{ m }}</option>
+        </select>
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click='submitPost'>Submit New Post</button>
 
         <hr>
-        <RouterLink to="/ViewPosts/">Click  here to return to Main Page</RouterLink>  
-       
+        <RouterLink to="/ViewPosts/">Click here to return to Main Page</RouterLink>  
     </div>
 </template>
 
